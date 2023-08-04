4e33ec584954e8e9c4790296e176648ab85d1f51
Vadim Vanatov <79990006611@yandex.ru>
2023-08-04 08:58:33 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by HOLY NADRUGANTIX on 04.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
